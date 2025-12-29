9ec9612802648f0e5a18e9a6ee505264e689172e
José David Gonzalez <gonzalez5.jose52@gmail.com>
2025-12-28 23:49:36 -0600

    feat: add footer

--- frontend/src/components/NewsletterBox.tsx
+++ frontend/src/components/NewsletterBox.tsx
@@ -8,13 +8,13 @@ const NewsletterBox = () => {
     }
 
   return (
-    <section className="text-center">
+    <section className="text-center" onSubmit={onSubmitHandler}>
         <p className="text-2xl font-medium text-gray-800">Subscribete para enterarte de nuestras novedades</p>
         <p className="text-gray-400 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
         <form className="w-full sm:1/2 flex items-center gap3 mx-auto my-6 border pl-3">
             <input type="email" placeholder="Ingresa tu correo" className="w-full sm:flex-1 outline-none" required/>
-            <button type="submit" className="bg-[black] text-white text-xs px-10 py-4">Subscribe</button>
+            <button type="submit" className="bg-[black] text-white text-xs px-10 py-4">Suscribirme</button>
         </form>
     </section>
   )
--- frontend/src/components/layouts/Footer.tsx
+++ frontend/src/components/layouts/Footer.tsx
@@ -1,9 +1,46 @@
-
+import { assets } from "../../assets/frontend_assets/assets";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} alt="logo" className="mb-5 w-32" />
+          <p className="w-full md:w-2/3 text-gray-600 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quod.
+          </p>
+        </div>
 
-export default Footer
+        <div>
+          <p className="text-xl font-medium mb-5">
+            COMPANY
+          </p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Inicio</li>
+            <li>Sobre nosotros</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+            </ul>
+       
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>123 Main St, Anytown, USA</li>
+            <li>(123) 456-7890</li>
+            <li>info@example.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr/>
+        <p className="py-5 text-sm text-center">© 2025 Your Company Name. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
